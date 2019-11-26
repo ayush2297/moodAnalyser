3c4943dddacbb5b83e457b0ec65dbf0d7bf354c4
ayush2297 <ayush.saraf47@gmail.com>
2019-11-26 15:25:08 +0530

    added: function to read parameterized class

--- src/main/java/com/moodanalyser/MoodAnalyzerFactory.java
+++ src/main/java/com/moodanalyser/MoodAnalyzerFactory.java
@@ -7,9 +7,32 @@ public class MoodAnalyzerFactory {
     public static MoodAnalyser createMoodAnalyzer() throws MoodAnalysisException {
         try{
             Class<?> moodAnalysisClass = Class.forName("com.moodanalyser.MoodAnalyser");
-            Constructor<?> moodConstructor = moodAnalysisClass.getConstructor(Integer.class);
+            Constructor<?> moodConstructor = moodAnalysisClass.getConstructor();
             Object moodObject;
             moodObject = moodConstructor.newInstance();
+            return (MoodAnalyser)moodObject;
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.NO_SUCH_METHOD,
+                    "enter correct method");
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.NO_SUCH_CLASS,
+                    "no such class please enter correct class" );
+        }
+        return null;
+    }
+
+    public static MoodAnalyser createMoodAnalyzer(String message) throws MoodAnalysisException {
+        try{
+            Class<?> moodAnalysisClass = Class.forName("com.moodanalyser.MoodAnalyser");
+            Constructor<?> moodConstructor = moodAnalysisClass.getConstructor(String.class);
+            Object moodObject;
+            moodObject = moodConstructor.newInstance(message);
             return (MoodAnalyser)moodObject;
         } catch (InstantiationException e) {
             e.printStackTrace();
