dfc2d2724d73a1a5d80803ed7dc983c2bc7e1bb9
ayush2297 <ayush.saraf47@gmail.com>
2019-11-26 10:23:25 +0530

    tc1.2:if sad then method returns sad els happy

--- src/test/java/com/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/moodanalyser/MoodAnalyserTest.java
@@ -5,4 +5,17 @@ import org.junit.Test;
 
 public class MoodAnalyserTest {
 
+    @Test
+    public void givenMood_whenSad_shouldReturnTrue(){
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("this is a sad message");
+        Assert.assertEquals("SAD",mood);
+    }
+
+    @Test
+    public void givenMood_whenHappy_shouldReturnTrue(){
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("this is a  message");
+        Assert.assertEquals("HAPPY",mood);
+    }
 }
