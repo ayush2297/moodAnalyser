10561a0e18993e09c66f059c163c69e2e7b1e819
ayush2297 <ayush.saraf47@gmail.com>
2019-11-26 10:15:02 +0530

    added: test where if message contains sad it returns SAD

--- src/test/java/com/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/moodanalyser/MoodAnalyserTest.java
@@ -5,4 +5,18 @@ import org.junit.Test;
 
 public class MoodAnalyserTest {
 
+    @Test
+    public void givenMood_whenSad_shouldReturnTrue(){
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("this is a sad message");
+        Assert.assertEquals("SAD",mood);
+    }
+
+    @Test
+    public void givenMood_whenHappy_shouldReturnTrue(){
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("this is a happy message");
+        Assert.assertEquals("HAPPY",mood);
+    }
+
 }
